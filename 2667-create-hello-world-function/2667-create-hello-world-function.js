60836afd66205472f23cc4076cff85af3b182755
Stevens Lloyd <78645466+sll0yd@users.noreply.github.com>
2025-03-08 18:00:28 +0100

    Time: 42 ms (80.37%), Space: 53.3 MB (15.56%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,16 +1,3 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-
-    
-    return function() {
-        return "Hello World"
-        
-    }
+const createHelloWorld = function() {
+    return () => "Hello World";
 };
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
